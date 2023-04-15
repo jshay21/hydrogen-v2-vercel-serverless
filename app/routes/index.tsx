0ab02e235d9cc13c4db7fa39ad4847485c7479f9
JShay21 <john@1121.us>
2023-04-15 11:24:32 -0700

    adding tut content

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,13 @@
+export const meta = () => {
+  return {
+    title: 'Hydrogen',
+    description: 'A custom storefront powered by Hydrogen',
+  };
+};
 export default function Index() {
-  <div>this is the index</div>;
+  return (
+    <div>
+      <h3>Hello from the home page!</h3>
+    </div>
+  );
 }
