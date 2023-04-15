0382bc1547f09ff477fbfac7038997c93cb7c9b1
JShay21 <john@1121.us>
2023-04-15 14:48:38 -0700

    many

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,12 @@ module.exports = {
   // appDirectory: 'app',
   ignoredRouteFiles: ['**/.*'],
   // watchPaths: ['./public'],
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.ts',
+  //server: process.env.NODE_ENV === 'development' ? undefined : './server.ts',
+
+  //adding server-dev from here 
+  // https://shopifyhydrogen.io/blog/how-to-deploy-shopify-hydrogen-2-to-vercel
+  server: process.env.NODE_ENV === 'development' ? './server-dev.ts' : './server.ts',
+
   serverBuildPath: 'api/index.js',
   /**
    * The following settings are required to deploy Hydrogen apps to Oxygen:
