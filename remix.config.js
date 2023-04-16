a846b95de083d5a0e0fa5c51d00e703efbf0539d
JShay21 <john@1121.us>
2023-04-15 22:46:16 -0700

    small change

--- remix.config.js
+++ remix.config.js
@@ -5,9 +5,10 @@ module.exports = {
   // watchPaths: ['./public'],
   //server: process.env.NODE_ENV === 'development' ? undefined : './server.ts',
 
-  //adding server-dev from here 
+  //adding server-dev from here
   // https://shopifyhydrogen.io/blog/how-to-deploy-shopify-hydrogen-2-to-vercel
-  server: process.env.NODE_ENV === 'development' ? './server-dev.ts' : './server.ts',
+  server:
+    process.env.NODE_ENV === 'development' ? './server-dev.ts' : './server.ts',
 
   serverBuildPath: 'api/index.js',
   /**
